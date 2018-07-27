541a8dc3ce48ffb78445a6dbee01db5cb496097f
Arttu Rousku <camelfunk@users.noreply.github.com>
2018-07-27 20:47:25 +0300

    4d6 - d6

--- chargen.py
+++ chargen.py
@@ -1,10 +1,19 @@
 from random import randint
 
 num_sides = 6       # 6-sided die
-num_dice = 3        # number of dice to roll
-num_final = 0       # added dice rolls together
+num_dice = 4   # number of dice to roll
+num_smallest = int()   # smallest roll
+num_final = int()   # added dice rolls together
+
 
 for i in range(num_dice):
-    num_final = num_final + (randint(1, num_sides))
+    num_random = (randint(1, num_sides))
+    num_final = num_final + num_random
 
-print(num_final)
+    if num_random < num_smallest:
+            num_smallest = num_random
+
+    elif num_smallest == int():
+            num_smallest = num_random
+
+print(num_final - num_smallest)  # removes the smallest dice roll of 4d6
