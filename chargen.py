b34919e9dd165e7daa2fe4fb5db3a59162e9604b
Arttu Rousku <camelfunk@users.noreply.github.com>
2018-07-27 17:52:43 +0300

    3d6

--- chargen.py
+++ chargen.py
@@ -1,1 +1,10 @@
-print ("pippeli") ;
+from random import randint
+
+num_sides = 6       # 6-sided die
+num_dice = 3        # number of dice to roll
+num_final = 0       # added dice rolls together
+
+for i in range(num_dice):
+    num_final = num_final + (randint(1, num_sides))
+
+print(num_final)
