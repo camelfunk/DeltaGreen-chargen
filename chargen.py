9c8b5d07e72e04a94c41aa29c3e746e4215706a6
Arttu Rousku <camelfunk@users.noreply.github.com>
2018-07-27 22:01:02 +0300

    stats loop

--- chargen.py
+++ chargen.py
@@ -1,19 +1,25 @@
 from random import randint
 
-num_sides = 6       # 6-sided die
-num_dice = 4   # number of dice to roll
-num_smallest = int()   # smallest roll
-num_final = int()   # added dice rolls together
+num_sides = 6  # 6-sided die
+num_dice = 4  # number of dice to roll
+num_smallest = int()  # smallest roll
+num_final = int()  # added dice rolls together
 
+stats = ['STR', 'DEX', 'CON', 'INT', 'POW', 'CHA']
 
-for i in range(num_dice):
-    num_random = (randint(1, num_sides))
-    num_final = num_final + num_random
+for x in stats:
+    for i in range(num_dice):
 
-    if num_random < num_smallest:
+        num_random = (randint(1, num_sides))
+        num_final = num_final + num_random
+
+        if num_random < num_smallest:
             num_smallest = num_random
 
-    elif num_smallest == int():
+        elif num_smallest == int():
             num_smallest = num_random
 
-print(num_final - num_smallest)  # removes the smallest dice roll of 4d6
+    num_final = num_final - num_smallest
+
+    print(x, num_final)
+    num_final = int()
