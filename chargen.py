8fd9557185d2fa39bc093961011372cbe6452144
Arttu Rousku <camelfunk@users.noreply.github.com>
2018-07-29 21:18:52 +0300

    generates stats based on profession

--- chargen.py
+++ chargen.py
@@ -2,6 +2,7 @@ from random import randint
 import random
 from random import gauss
 from data import *
+from collections import OrderedDict
 
 print(random.choice(sex))
 
@@ -55,16 +56,162 @@ print("BP", breakingPoint)
 maxValue = max(stats.values())
 statGenerate = random.choice([k for k, v in stats.items() if v == maxValue])
 
-if statGenerate == "STR":
-    print(random.choice(strProfession))
-elif statGenerate == "DEX":
-    print(random.choice(dexProfession))
-elif statGenerate == "CON":
-    print(random.choice(conProfession))
-elif statGenerate == "INT":
-    print(random.choice(intProfession))
-elif statGenerate == "POW":
-    print(random.choice(powProfession))
-else:
-    print(random.choice(chaProfession))
 
+# Skill generation based on profession
+
+
+if statGenerate == "STR":
+    ProfessionGenerate = random.choice(strProfession)
+    print(ProfessionGenerate)
+
+    if ProfessionGenerate == profession[7]:
+        specopskillreplacement()
+
+    elif ProfessionGenerate == profession[8]:
+        criminalskillreplacement()
+
+    elif ProfessionGenerate == profession[9]:
+        firefighterskillreplacement()
+
+    elif ProfessionGenerate == profession[20]:
+        policeskillreplacement()
+
+    elif ProfessionGenerate == profession[22]:
+        soldierskillreplacement()
+
+    else:
+        marineskillreplacement()
+
+
+elif statGenerate == "DEX":
+    ProfessionGenerate = random.choice(dexProfession)
+    print(ProfessionGenerate)
+
+    if ProfessionGenerate == profession[5]:
+        physicianskillreplacement()
+
+    elif ProfessionGenerate == profession[8]:
+        criminalskillreplacement()
+
+    elif ProfessionGenerate == profession[9]:
+        firefighterskillreplacement()
+
+    else:
+        pilotskillreplacement()
+
+elif statGenerate == "CON":
+    ProfessionGenerate = random.choice(conProfession)
+    print(ProfessionGenerate)
+
+    if ProfessionGenerate == profession[4]:
+        fagentskillreplacement()
+
+    elif ProfessionGenerate == profession[7]:
+        specopskillreplacement()
+
+    elif ProfessionGenerate == profession[9]:
+        firefighterskillreplacement()
+
+    elif ProfessionGenerate == profession[20]:
+        policeskillreplacement()
+
+    elif ProfessionGenerate == profession[22]:
+        soldierskillreplacement()
+
+    else:
+        marineskillreplacement()
+
+
+elif statGenerate == "INT":
+    ProfessionGenerate = random.choice(intProfession)
+    print(ProfessionGenerate)
+
+    if ProfessionGenerate == profession[0]:
+        anthroskillreplacement()
+
+    elif ProfessionGenerate == profession[1]:
+        historianskillreplacement()
+
+    elif ProfessionGenerate == profession[2] or ProfessionGenerate == profession[3]:
+        compsciskillreplacement()
+
+    elif ProfessionGenerate == profession[5]:
+        physicianskillreplacement()
+
+    elif ProfessionGenerate == profession[6]:
+        scientistskillreplacement()
+
+    elif ProfessionGenerate == profession[10]:
+        fsoskillreplacement()
+
+    elif ProfessionGenerate == profession[11]:
+        ianalystskillreplacement()
+
+    elif ProfessionGenerate == profession[12]:
+        icoskillreplacement()
+
+    elif ProfessionGenerate == profession[13] or ProfessionGenerate == profession[14]:
+        lawyerskillreplacement()
+
+    elif ProfessionGenerate == profession[15]:
+        mediaspecialistskillreplacement()
+
+    elif ProfessionGenerate == profession[16] or ProfessionGenerate == profession[17]:
+        nurseskillreplacement()
+
+    elif ProfessionGenerate == profession[18] or ProfessionGenerate == profession[19]:
+        pilotskillreplacement()
+
+    else:
+        progmgrskillreplacement()
+
+
+elif statGenerate == "POW":
+    ProfessionGenerate = random.choice(powProfession)
+    print(ProfessionGenerate)
+
+    if ProfessionGenerate == profession[4]:
+        fagentskillreplacement()
+
+    elif ProfessionGenerate == profession[5]:
+        physicianskillreplacement()
+
+    elif ProfessionGenerate == profession[7]:
+        specopskillreplacement()
+
+    elif ProfessionGenerate == profession[12]:
+        icoskillreplacement()
+
+    elif ProfessionGenerate == profession[16] or ProfessionGenerate == profession[17]:
+        nurseskillreplacement()
+
+    else:
+        policeskillreplacement()
+
+
+else:
+    ProfessionGenerate = random.choice(chaProfession)
+    print(ProfessionGenerate)
+
+    if ProfessionGenerate == profession[4]:
+        fagentskillreplacement()
+
+    elif ProfessionGenerate == profession[10]:
+        fsoskillreplacement()
+
+    elif ProfessionGenerate == profession[12]:
+        icoskillreplacement()
+
+    elif ProfessionGenerate == profession[13] or ProfessionGenerate == profession[14]:
+        lawyerskillreplacement()
+
+    elif ProfessionGenerate == profession[15]:
+        mediaspecialistskillreplacement()
+
+    elif ProfessionGenerate == profession[16] or ProfessionGenerate == profession[17]:
+        nurseskillreplacement()
+
+    else:
+        progmgrskillreplacement()
+
+print(skills)
--- data.py
+++ data.py
@@ -1,3 +1,121 @@
+import random
+
+
+def anthroskillreplacement():
+    skills.update(anthropologistSkills)
+    skills.update(random.sample(list(anthropologistOptionalSkills.items()), 2))
+    # 4bonds
+
+
+def historianskillreplacement():
+    skills.update(historianSkills)
+    skills.update(random.sample(list(historianOptionalSkills.items()), 2))
+    # 4bonds
+
+
+def compsciskillreplacement():
+    skills.update(computerScientistSkills)
+    skills.update(random.sample(list(computerScientistOptionalSkills.items()), 4))
+    # 3bonds
+
+
+def fagentskillreplacement():
+    skills.update(federalAgentSkills)
+    skills.update(random.sample(list(federalAgentOptionalSkills.items()), 1))
+    # 3bonds
+
+
+def physicianskillreplacement():
+    skills.update(physicianSkills)
+    skills.update(random.sample(list(physicianOptionalSkills.items()), 2))
+    # 3bonds
+
+
+def scientistskillreplacement():
+    skills.update(scientistSkills)
+    skills.update(random.sample(list(scientistOptionalSkills.items()), 3))
+    # 4bonds
+
+
+def specopskillreplacement():
+    skills.update(specOpSkills)
+    # 2bonds
+
+
+def criminalskillreplacement():
+    skills.update(criminalSkills)
+    skills.update(random.sample(list(criminalOptionalSkills.items()), 2))
+    # 4bonds
+
+
+def firefighterskillreplacement():
+    skills.update(firefighterSkills)
+    # 3bonds
+
+
+def fsoskillreplacement():
+    skills.update(fsoSkills)
+    # 3bonds
+
+
+def ianalystskillreplacement():
+    skills.update(iAnalystSkills)
+    # 3bonds
+
+
+def icoskillreplacement():
+    skills.update(icoSkills)
+    # 2bonds
+
+
+def lawyerskillreplacement():
+    skills.update(lawyerSkills)
+    skills.update(random.sample(list(lawyerOptionalSkills.items()), 4))
+    # 4bonds
+
+
+def mediaspecialistskillreplacement():
+    skills.update(mediaSpecialistSkills)
+    skills.update(random.sample(list(mediaSpecialistOptionalSkills.items()), 5))
+    # 4bonds
+
+
+def nurseskillreplacement():
+    skills.update(nurseSkills)
+    skills.update(random.sample(list(nurseOptionalSkills.items()), 2))
+    # 4bonds
+
+
+def pilotskillreplacement():
+    skills.update(pilotSkills)
+    skills.update(random.sample(list(pilotOptionalSkills.items()), 2))
+    # 3bonds
+
+
+def policeskillreplacement():
+    skills.update(policeSkills)
+    skills.update(random.sample(list(policeOptionalSkills.items()), 1))
+    # 3bonds
+
+
+def progmgrskillreplacement():
+    skills.update(progMgrSkills)
+    skills.update(random.sample(list(progMgrOptionalSkills.items()), 1))
+    # 4bonds
+
+
+def soldierskillreplacement():
+    skills.update(soldierSkills)
+    skills.update(random.sample(list(soldierOptionalSkills.items()), 3))
+    # 4bonds
+
+
+def marineskillreplacement():
+    skills.update(marineSkills)
+    skills.update(random.sample(list(marineOptionalSkills.items()), 3))
+    # 4bonds
+
+
 num_sides = 6  # 6-sided die
 num_dice = 4  # number of dice to roll
 num_smallest = int()  # smallest roll
@@ -13,10 +131,13 @@ profession = ['Anthropologist', 'Historian', 'Computer Scientist', 'Engineer', '
               'Intelligence Analyst', 'Intelligence Case Officer', 'Lawyer', 'Business Executive', 'Media Specialist',
               'Nurse', 'Paramedic', 'Pilot', 'Sailor', 'Police Officer', 'Program Manager', 'Soldier', 'Marine']
 
-skills = {'Accounting': 10, 'Alertness': 20, 'Anthropology': 0, 'Archeology': 0, 'Art (Type)': 0, 'Athletics': 30,
-          'Artillery': 0, 'Bureaucracy': 10, 'Computer Science': 0, 'Craft (Type)': 0, 'Criminology': 10,
+# base stats for skills
+
+
+skills = {'Accounting': 10, 'Alertness': 20, 'Anthropology': 0, 'Archeology': 0, 'Art (Type)': 0, 'Artillery': 0,
+          'Athletics': 30, 'Bureaucracy': 10, 'Computer Science': 0, 'Craft (Type)': 0, 'Criminology': 10,
           'Demolitions': 0, 'Disguise': 10, 'Dodge': 30, 'Drive': 20, 'Firearms': 20, 'First Aid': 10,
-          'Foreign Language (Type)': 0, 'Forensics': 0, 'Heavy Machinery': 10, 'Heavy Weapons': 0, 'History': 10,
+          'Forensics': 0, 'Heavy Machinery': 10, 'Heavy Weapons': 0, 'History': 10,
           'HUMINT': 10, 'Law': 0, 'Medicine': 0, 'Melee Weapons': 30, 'Military Science (Type)': 0,
           'Navigate': 10, 'Occult': 10, 'Persuade': 20, 'Pharmacy': 0, 'Pilot (Type)': 0, 'Psychotherapy': 10,
           'Ride': 10, 'Science (Type)': 0, 'Search': 20, 'SIGINT': 0, 'Stealth': 10, 'Surgery': 0,
@@ -39,3 +160,101 @@ powProfession = [profession[4], profession[5], profession[7], profession[12], pr
 chaProfession = [profession[4], profession[10], profession[12], profession[13], profession[14], profession[15],
                  profession[16], profession[17], profession[21]]
 
+# Updated over base skills after profession generated
+
+anthropologistSkills = {'Anthropology': 50, 'Bureaucracy': 40, 'History': 60, 'Occult': 40, 'Persuade': 40,
+                        'Foreign Language (choose first)': 50, 'Foreign Language (choose second)': 40}
+anthropologistOptionalSkills = {'Archeology': 40, 'HUMINT': 50, 'Navigate': 50, 'Ride': 50, 'Search': 60,
+                                'Survival': 50}
+
+historianSkills = {'Archeology': 50, 'Bureaucracy': 40, 'History': 60, 'Occult': 40, 'Persuade': 40,
+                   'Foreign Language (choose first)': 50, 'Foreign Language (choose second)': 40}
+historianOptionalSkills = {'Anthropology': 40, 'HUMINT': 50, 'Navigate': 50, 'Ride': 50, 'Search': 60,
+                           'Survival': 50}
+
+computerScientistSkills = {'Computer Science': 60, 'Craft (Electrician)': 30, 'Craft (Mechanic)': 30,
+                           'Craft (Microelectronics)': 40, 'Science (Mathematics)': 40, 'SIGINT': 40}
+computerScientistOptionalSkills = {'Accounting': 50, 'Bureaucracy': 50, 'Craft (choose one)': 40,
+                                   'Foreign Language (choose one)': 40, 'Heavy Machinery': 50, 'Law': 40,
+                                   'Science (choose one)': 40}
+
+federalAgentSkills = {'Alertness': 50, 'Bureaucracy': 40, 'Criminology': 50, 'Drive': 50, 'Firearms': 50,
+                      'Forensics': 30, 'HUMINT': 60, 'Law': 30, 'Persuade': 50, 'Search': 50, 'Unarmed Combat': 60}
+federalAgentOptionalSkills = {'Accounting': 60, 'Computer Science': 50, 'Foreign Language (choose one)': 50,
+                              'Heavy Weapons': 50, 'Pharmacy': 50}
+
+physicianSkills = {'Bureaucracy': 50, 'First Aid': 60, 'Medicine': 60, 'Persuade': 40, 'Pharmacy': 50,
+                   'Science (Biology)': 60, 'Search': 40}
+physicianOptionalSkills = {'Forensics': 50, 'Psychotherapy': 60, 'Science (choose one)': 50, 'Surgery': 50}
+
+scientistSkills = {'Bureaucracy': 40, 'Computer Science': 40, 'Science (choose first)': 60,
+                   'Science (choose second)': 50, 'Science (choose third)': 50}
+scientistOptionalSkills = {'Accounting': 50, 'Craft (choose one)': 40, 'Foreign Language (choose one)': 40,
+                           'Forensics': 40, 'Law': 40, 'Pharmacy': 40}
+
+specOpSkills = {'Alertness': 60, 'Athletics': 60, 'Demolitions': 40, 'Firearms': 60, 'Heavy Weapons': 50,
+                'Melee Weapons': 50, 'Military Science (Land)': 60, 'Navigate': 50, 'Stealth': 50,
+                'Survival': 50, 'Swim': 50, 'Unarmed Combat': 60}
+
+criminalSkills = {'Alertness': 50, 'Criminology': 60, 'Dodge': 40, 'Drive': 50, 'Firearms': 40, 'Law': 40,
+                  'Melee Weapons': 40, 'Persuade': 50, 'Stealth': 50, 'Unarmed Combat': 50}
+criminalOptionalSkills = {'Craft (Locksmithing)': 40, 'Demolitions': 40, 'Disguise': 50,
+                          'Foreign Language (choose one)': 40, 'Forensics': 40, 'HUMINT': 50, 'Navigate': 50,
+                          'Occult': 50, 'Pharmacy': 40}
+
+firefighterSkills = {'Alertness': 50, 'Athletics': 60, 'Craft (Electrician)': 40, 'Craft (Mechanic)': 40,
+                     'Demolitions': 50, 'Drive': 50, ' First Aid': 50, 'Forensics': 40, 'Heavy Machinery': 50,
+                     'Navigate': 50, 'Search': 40}
+
+fsoSkills = {'Accounting': 40, 'Anthropology': 40, 'Bureaucracy': 60, 'Foreign Language (choose first)': 50,
+             'Foreign Language (choose second)': 50, 'Foreign Language (choose third)': 40, 'History': 40,
+             'HUMINT': 50, 'Law': 40, 'Persuade': 50}
+
+iAnalystSkills = {'Anthropology': 40, 'Bureaucracy': 50, 'Computer Science': 40, 'Criminology': 40,
+                  'Foreign Language (choose first)': 50, 'Foreign Language (choose second)': 50,
+                  'Foreign Language (choose third)': 40, 'History': 40, 'HUMINT': 50, 'SIGINT': 40}
+
+icoSkills = {'Alertness': 50, 'Bureaucracy': 40, 'Criminology': 50, 'Disguise': 50, 'Drive': 40, 'Firearms': 40,
+             'Foreign Language (choose first)': 50, 'Foreign Language (choose second)': 40, 'HUMINT': 60,
+             'Persuade': 60, 'SIGINT': 40, 'Stealth': 50, 'Unarmed Combat': 50}
+
+lawyerSkills = {'Accounting': 50, 'Bureaucracy': 50, 'HUMINT': 40, 'Persuade': 60}
+lawyerOptionalSkills = {'Computer Science': 50, 'Criminology': 60, 'Foreign Language (choose one)': 50, 'Law': 50,
+                        'Pharmacy': 50}
+
+mediaSpecialistSkills = {'Art (choose one)': 60, 'History': 40, 'HUMINT': 40, 'Persuade': 50}
+mediaSpecialistOptionalSkills = {'Anthropology': 40, 'Archeology': 40, 'Art (choose second)': 40, 'Bureaucracy': 50,
+                                 'Computer Science': 40, 'Criminology': 50, 'Foreign Language (choose one)': 40,
+                                 'Law': 40, 'Military Science (choose one)': 40, 'Occult': 50,
+                                 'Science (choose one)': 40}
+
+nurseSkills = {'Alertness': 40, 'Bureaucracy': 40, 'First Aid': 60, 'HUMINT': 40, 'Medicine': 40, 'Persuade': 40,
+               'Pharmacy': 40, 'Science (Biology)': 40}
+nurseOptionalSkills = {'Drive': 60, 'Forensics': 40, 'Navigate': 50, 'Psychotherapy': 50, 'Search': 60}
+
+pilotSkills = {'Alertness': 60, 'Bureaucracy': 30, 'Craft (Electrician)': 40, 'Craft (Mechanic)': 40, 'Navigate': 50,
+               'Pilot (choose one)': 60, 'Science (Meteorology)': 40, 'Swim': 40}
+pilotOptionalSkills = {'Foreign Language (choose one)': 50, 'Pilot (choose second)': 50, 'Heavy Weapons': 50,
+                       'Military Science (choose one)': 50}
+
+policeSkills = {'Alertness': 60, 'Bureaucracy': 40, 'Criminology': 40, 'Drive': 50, 'Firearms': 40, 'First Aid': 30,
+                'HUMINT': 50, 'Law': 30, 'Melee Weapons': 50, 'Navigate': 40, 'Persuade': 40, 'Search': 40,
+                'Unarmed Combat': 60}
+policeOptionalSkills = {'Forensics': 50, 'Heavy Machinery': 60, 'Heavy Weapons': 50, 'Ride': 60}
+
+progMgrSkills = {'Accounting': 60, 'Bureaucracy': 60, 'Computer Science': 50, 'Criminology': 30,
+                 'Foreign Language (choose one)': 50, 'History': 40, 'Law': 40, 'Persuade': 50}
+progMgrOptionalSkills = {'Anthropology': 30, 'Art (choose one)': 30, 'Craft (choose one)': 30,
+                         'Science (choose one)': 30}
+
+soldierSkills = {'Alertness': 50, 'Athletics': 50, 'Bureaucracy': 30, 'Drive': 40, 'Firearms': 40, 'First Aid': 40,
+                 'Military Science (Land)': 40, 'Navigate': 40, 'Persuade': 30, 'Unarmed Combat': 50}
+soldierOptionalSkills = {'Artillery': 40, 'Computer Science': 40, 'Craft (choose one)': 40, 'Demolitions': 40,
+                         'Foreign Language (choose one)': 40, 'Heavy Machinery': 50, 'Heavy Weapons': 40, 'Search': 60,
+                         'SIGINT': 40, 'Swim': 60}
+
+marineSkills = {'Alertness': 50, 'Athletics': 50, 'Bureaucracy': 30, 'Drive': 40, 'Firearms': 40, 'First Aid': 40,
+                'Military Science (Sea)': 40, 'Navigate': 40, 'Persuade': 30, 'Unarmed Combat': 50}
+marineOptionalSkills = {'Artillery': 40, 'Computer Science': 40, 'Craft (choose one)': 40, 'Demolitions': 40,
+                        'Foreign Language (choose one)': 40, 'Heavy Machinery': 50, 'Heavy Weapons': 40, 'Search': 60,
+                        'SIGINT': 40, 'Swim': 60}
